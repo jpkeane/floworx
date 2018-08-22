b4c5c6e3098dafd4c7fd6bc7025e29cf6d65b365
James Keane <james.keane@CY-PC-0002.local>
2018-08-22 23:43:08 +0100

    Base template added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 # Application-wide Helper
 module ApplicationHelper
+  # Returns the full title on a per-page basis.
+  def full_title(page_title = '')
+    base_title = 'Reference'
+    if page_title.empty?
+      base_title
+    else
+      page_title + ' | ' + base_title
+    end
+  end
 end
