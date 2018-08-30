962dc9cb6aaf83473955bb50b24dd118ad0c89a2
James Keane <james.keane@CY-PC-0002.local>
2018-08-30 11:17:45 +0100

    Added Client view UI

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :teams, only: %i[index show]
   resources :staff, only: %i[index show]
+  resources :clients, only: %i[index show]
   resources :grades, only: %i[index show]
   root 'front_pages#home'
 end
