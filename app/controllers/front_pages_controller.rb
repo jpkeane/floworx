12fd9f8e0c21ade422da3a6aba8648c1def802d2
James Keane <james.keane@CY-PC-0002.local>
2018-09-04 15:19:13 +0100

    Login Test

--- app/controllers/front_pages_controller.rb
+++ app/controllers/front_pages_controller.rb
@@ -1,4 +1,6 @@
 class FrontPagesController < ApplicationController
   layout 'front_pages'
   def home; end
+
+  def test; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  get 'dashboard', to: 'dashboards#index'
-
-  resources :teams, only: %i[index show]
-  resources :staff, only: %i[index show]
-  resources :clients
-  resources :grades, only: %i[index show]
-  resources :projects, only: %i[index show]
-  resources :roles, only: %i[index show]
-  resources :skills, only: %i[index show]
-  resources :engagements, only: %i[index show]
+  get 'dashboard', to: 'front_pages#test'
+  # get 'dashboard', to: 'dashboards#index'
+  #
+  # resources :teams, only: %i[index show]
+  # resources :staff, only: %i[index show]
+  # resources :clients
+  # resources :grades, only: %i[index show]
+  # resources :projects, only: %i[index show]
+  # resources :roles, only: %i[index show]
+  # resources :skills, only: %i[index show]
+  # resources :engagements, only: %i[index show]
   root 'front_pages#home'
 end
